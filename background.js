dbd173e1857958b8336e01e00c035b6d51007d4d
kmelon55 <kimkyoungmo2869@gmail.com>
2023-06-05 00:48:33 +0900

    Add message passing

--- background.js
+++ background.js
@@ -1,32 +1,47 @@
-function executeSnippet(snippet) {
-  // content script에 스니펫을 실행하도록 메시지를 보냅니다.
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const activeTab = tabs[0];
-    chrome.scripting
-      .executeScript({
-        target: { tabId: activeTab.id },
-        func: createSnippetFunction(snippet.code),
-      })
-      .then(() => console.log("injected a function"));
-  });
-
-  console.log("Executing snippet:", snippet);
-}
-function createSnippetFunction(code) {
-  return new Function(code);
-}
 function checkSnippetExecution(url) {
   chrome.storage.sync.get("snippets", function (result) {
     const snippets = result.snippets || [];
     snippets.forEach(function (snippet) {
       if (snippet.url === url) {
-        executeSnippet(snippet);
+        // (async () => {
+        //   const [tab] = await chrome.tabs.query({
+        //     active: true,
+        //     lastFocusedWindow: true,
+        //   });
+        // })(
+        //   chrome.scripting.executeScript(
+        //     {
+        //       target: { tabId: tab.id },
+        //       func: `(function executeSnippet() {
+        //       ${snippet.code}
+        //     })();`,
+        //     },
+        //     function () {
+        //       console.log("Executed snippet:", snippet);
+        //     }
+        //   )
+        // );
+
+        // message passing
+        (async () => {
+          const [tab] = await chrome.tabs.query({
+            active: true,
+            lastFocusedWindow: true,
+          });
+          const response = await chrome.tabs.sendMessage(tab.id, {
+            action: "executeSnippet",
+            snippet: snippet,
+          });
+          console.log(response);
+        })();
       }
     });
   });
 }
 
-chrome.webNavigation.onDOMContentLoaded.addListener(function (details) {
-  const url = details.url;
-  checkSnippetExecution(url);
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete") {
+    const url = tab.url;
+    checkSnippetExecution(url);
+  }
 });
--- content-script.js
+++ content-script.js
@@ -1,11 +1,57 @@
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//   if (message.action === "executeSnippet") {
+//     try {
+//       const { code } = message.snippet;
+
+//       const blob = new Blob([code], { type: "text/javascript" });
+//       const worker = new Worker(window.URL.createObjectURL(blob));
+
+//       worker.onmessage = function (event) {
+//         // 결과를 받아서 메인 스레드에서 처리
+//         if (event.data === "alert") {
+//           alert("Alert from Worker");
+//         }
+//         sendResponse({ success: true });
+//         worker.terminate();
+//       };
+
+//       worker.onerror = function (error) {
+//         sendResponse({ success: false, error: error.message });
+//         worker.terminate();
+//       };
+//     } catch (error) {
+//       sendResponse({ success: false, error: error.message });
+//     }
+//   }
+//   return true; // 비동기 응답을 허용
+// });
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === "executeSnippet") {
+    console.log("execute script");
+    try {
+      const code = message.snippet.code;
+      console.log(message.snippet.code);
+
+      const script = document.createElement("script");
+      script.textContent = code;
+      console.log(code);
+      document.body.appendChild(script);
+
+      sendResponse({ success: true });
+    } catch (error) {
+      sendResponse({ success: false, error: error.message });
+    }
+  }
+  return true; // 비동기 응답을 허용
+});
+
 // const elementWithRootId = document.getElementById("root");
 
 // if (elementWithRootId) {
-//   // 첫 번째 자식 요소를 선택합니다.
 //   const firstChild = elementWithRootId.firstChild;
 
 //   if (firstChild) {
-//     // 첫 번째 자식 요소에 대해 display를 none으로 설정합니다.
 //     firstChild.style.display = "none";
 //     console.log("success!");
 //   } else {
@@ -14,17 +60,3 @@
 // } else {
 //   console.error("No element found with the given root ID.");
 // }
-
-// content script에서 스니펫 실행
-function executeSnippetInContentScript(snippet) {
-  const script = document.createElement("script");
-  script.textContent = snippet.code;
-  document.body.appendChild(script);
-}
-
-// background.js에서 메시지 수신 및 스니펫 실행
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.snippet) {
-    executeSnippetInContentScript(request.snippet);
-  }
-});
